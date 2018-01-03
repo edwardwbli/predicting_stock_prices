7854aada2caa7199984caddecf0923a46173e2ea
edwardwbli <edward.w.b.li@gmail.com>
2018-01-03 11:08:34 +0800

    change maplot to print image only

--- demo.py
+++ demo.py
@@ -1,10 +1,12 @@
 import csv
 import numpy as np
 from sklearn.svm import SVR
+import matplotlib as mp
+mp.use('Agg')
 import matplotlib.pyplot as plt
 
 
-plt.switch_backend('newbackend')  
+#plt.switch_backend('newbackend')  
 
 
 
@@ -22,14 +24,14 @@ def get_data(filename):
 
 def predict_price(dates, prices, x):
 	dates = np.reshape(dates,(len(dates), 1)) # converting to matrix of n X 1
-
+	print "her1"
 	svr_lin = SVR(kernel= 'linear', C= 1e3)
 	svr_poly = SVR(kernel= 'poly', C= 1e3, degree= 2)
 	svr_rbf = SVR(kernel= 'rbf', C= 1e3, gamma= 0.1) # defining the support vector regression models
 	svr_rbf.fit(dates, prices) # fitting the data points in the models
 	svr_lin.fit(dates, prices)
 	svr_poly.fit(dates, prices)
-
+	print "her2"
 	plt.scatter(dates, prices, color= 'black', label= 'Data') # plotting the initial datapoints 
 	plt.plot(dates, svr_rbf.predict(dates), color= 'red', label= 'RBF model') # plotting the line made by the RBF kernel
 	plt.plot(dates,svr_lin.predict(dates), color= 'green', label= 'Linear model') # plotting the line made by linear kernel
@@ -38,12 +40,12 @@ def predict_price(dates, prices, x):
 	plt.ylabel('Price')
 	plt.title('Support Vector Regression')
 	plt.legend()
-	plt.show()
+	plt.savefig('stocks.png')
 
 	return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
 
 get_data('aapl.csv') # calling get_data method by passing the csv file to it
-#print "Dates- ", dates
-#print "Prices- ", prices
+print "Dates- ", dates
+print "Prices- ", prices
 
 predicted_price = predict_price(dates, prices, 29)  
